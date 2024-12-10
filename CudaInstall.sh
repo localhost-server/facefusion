03fbc73e4837e8a8cfaa07d4d00a09774d8c06ef
localhost-server <99524394+localhost-server@users.noreply.github.com>
2024-12-10 10:08:32 +0000

    adding cuda

--- CudaInstall.sh
+++ CudaInstall.sh
@@ -17,7 +17,7 @@ conda create --name facefusion python=3.12 -y
 conda activate facefusion
 
 # Install necessary dependencies
-python install.py --onnxruntime default
+python install.py --onnxruntime cuda
 conda install conda-forge::cuda-runtime=12.4.1 conda-forge::cudnn=9.2.1.18 -y
 pip install tensorrt==10.6.0 --extra-index-url https://pypi.nvidia.com
 
